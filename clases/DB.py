489cc4ba7607cc1cf1286624ad8d17430f501cec
Timur Sorokin <timursorokin94@gmail.com>
2023-12-13 18:59:06 +0100

    clase DB

--- clases/DB.py
+++ clases/DB.py
@@ -7,90 +7,74 @@ class DB:
         self.pwd = os.environ['DB_PASSWORD']
         self.conn = None 
         try:
-            self.conn = oracledb.connect(user=usr,password=pwd,dsn=cs)
+            self.conn = oracledb.connect(user=usr,password=pwd,dsn=CS)
         except Exception as e:
             print(f"Error al establecer conexión:{e}")
             exit(-1)
 
-    def exec_querry(querry: str, msg="Error generico")->list:
-        output = []
+    #Generar bases de datos
+    def crear_tablas(self):
+        #Clientes
+        with open("sql/create_tables.txt") as ifs:
+            lines = ifs.readlines()
+        for line in lines:
+            if line:
+                try:
+                    conn.cursor.execute(line)
+                except Exception as e:
+                    print(f"Error al crear tabla clientes: {e}")
+                    fail = True
+        
+    def llenar_tablas(self):
+
+        #Propiedades
+        num_entradas = 10
+        random.seed(69)
+        fail = False
+        clientes = propiedades = []
+        query = "INSERT INTO Cliente VALUES (:1, :2)"
+        #Clientes - Genero lista de tupla 
+        for i in range(0,num_entradas,1):
+            clientes.append((i,random.randint(0,999)))
+            propiedades.append((i,random.randint(0,999)))
+        
+        savepoint = f"SP_{random.randint(0,99)}"
+        cursor.execute(f"SAVEPOINT {savepoint}")
         try:
-            cur.execute(querry)
-            output = cur.fetchall()
+            conn.cursor.executemany(query,clientes)
+            conn.cursor.executemany(query,propiedades)
         except Exception as e:
-            print(f"Error al consultar libros: {e}")
-        return output
+            fail = True
+            print(f"Ha habido un error al desplegar base de datos {e}")
+            cursor.execute(f"ROLLBACK TO SAVEPOINT {savepoint}")
+        finally:
+            if not fail:
+                conn.commit() #Puede lanzar excepcion
+        
 
-
-#Generar bases de datos
-def crear_tablas(self):
-    #Clientes
-    with open("sql/create_tables.txt") as ifs:
-        lines = ifs.readlines()
-    for line in lines:
-        if line:
+    def borra_tablas(self):
+        with open("sql/drop_tables.txt",'r') as input:
+            lines = input.readlines()
+        for line in lines:
             try:
                 conn.cursor.execute(line)
             except Exception as e:
-                print(f"Error al crear tabla clientes: {e}")
-                fail = True
-    
-    #Propiedades
-    with open("sql/create_tables.txt") as ifs:
-        lines = ifs.readlines()
-    for line in lines:
-        if line:
-            try:
-                conn.cursor.execute(line)
-            except Exception as e:
-                print(f"Error al crear tabla clientes: {e}")
-                fail = True
+                print(f"Ha habido un error {e}")
+                break
+               
+                
+    def imprimir_tablas():
+        with open("sql/print_tables.txt",'r') as input:
+            lines = input.readlines()
+        for line in lines:
+           try:
+               conn.cursor.execute(line)
+               tabla = from_db_cursor(cursor)
+               nombre_tabla = line.split(' ')[-1].strip()
+               print(f"Tabla: {nombre_tabla}")
+               print(f"{tabla}\n")
+           except Exception as e:
+                print(f"Error al consultar las tablas: {e}")
 
-    #CompraVenta
-    with open("sql/create_tables.txt") as ifs:
-        lines = ifs.readlines()
-    for line in lines:
-        if line:
-            try:
-                conn.cursor.execute(line)
-            except Exception as e:
-                print(f"Error al crear tabla clientes: {e}")
-                fail = True
-    
-    #Servicio
-    with open("sql/create_tables.txt") as ifs:
-        lines = ifs.readlines()
-    for line in lines:
-        if line:
-            try:
-                conn.cursor.execute(line)
-            except Exception as e:
-                print(f"Error al crear tabla clientes: {e}")
-                fail = True
-  
-def borra_tablas():
-    with open("sql/drop_tables.txt",'r') as input:
-        lines = input.readlines()
-    for line in lines:
-        try:
-            conn.cursor.execute(line)
-        except Exception as e:
-            print(f"Ha habido un error {e}")
-            break
-           
-            
-def imprimir_tablas():
-    with open("sql/print_tables.txt",'r') as input:
-        lines = input.readlines()
-    for line in lines:
-       try:
-           conn.cursor.execute(line)
-           tabla = from_db_cursor(cursor)
-           nombre_tabla = line.split(' ')[-1].strip()
-           print(f"Tabla: {nombre_tabla}")
-           print(f"{tabla}\n")
-       except Exception as e:
-            print(f"Error al consultar las tablas: {e}")
-
-    
+        
 
